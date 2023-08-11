4105d1b67c28e4223f0a9f38571fcc4717c5758c
mathrews <mateusps4gg@gmail.com>
2023-08-11 16:14:15 -0300

    funções

---
+++ aulas-GG/modulo-F/aula02/anot.md
@@ -0,0 +1,18 @@
+# Funções em JS:
+<br>
+
+- O js é uma linguagem do paradigma `funcional`.
+<br>
+
+- Temos funções na nossa vida. Como limpar o quarto por exemplo. Isso tudo pode ser chamado de tarefas, **rotinas** ou funções.
+<br>
+
+- A função funciona assim: pedimos algo ao sistema e entregamos um dado a ele, ele realiza o processo para conseguir esse algo relacionado a esse dado, e nos retorna esse algo.
+<br>
+
+- `Quem pede algo ao sistema: chamada; o dado entregue ao sistema para realizar o processo: parâmetro; o processo: ação; entrega de algo resultante da ação: retorno.`
+<br>
+
+- Funções: **Ações** executadas assim que são **chamadas** ou em decorrência de algum **evento**. A função pode receber **parâmetros** e **retornar** um resultado.
+<br>
+
---
+++ aulas-GG/modulo-F/aula02/scripts.js
@@ -0,0 +1,49 @@
+
+'FUNÇÃO: 01'
+// function parimpar(n) {
+//     if (n % 2 == 0) {
+//         return 'par';
+//     } else {
+//         return 'impar';
+//     }
+// }
+
+// let res = parimpar(11);
+
+// console.log(res);
+
+'FUNÇÃO: 02'
+// function soma(a = 0, b = 0)/*definir antecipadamente parâmetros para evitar erros*/{
+//     return a + b
+// }
+
+// console.log(soma(12, 3));
+
+'FUNÇÃO: 03'
+// let v = function(x) {
+//     return x * 2;
+// }
+
+// console.log( v(5) );
+
+'FUNÇÃO: 04'
+// function fatorial(n){
+//     let fat = 1;
+//     for(let i = n; i >= 1; i--){
+//         fat *= i;
+//     }
+//     return fat;
+// }
+
+// console.log(fatorial(4));
+
+'FUNÇÃO: 04 RECUSIVA'
+function fatorial(n) {
+    if (n == 1) {
+        return 1
+    } else {
+        return n * fatorial(n - 1)
+    }
+}
+
+console.log(fatorial(5));
