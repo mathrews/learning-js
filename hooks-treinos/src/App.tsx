00e4840b4a90151910b01fdc5e7870cb79b6271d
mathrews <mateusps4gg@gmail.com>
2023-09-21 18:38:00 -0300

    projeto hook treinos criado

---
+++ hooks-treinos/src/App.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react"
+
+function App() {
+  const [count, setCount] = useState(0) //a quantidade de vezes que um hook é chamado, nunca pode mudar
+  const [theme, setTheme] = useState('dark')
+
+  // const [state, setState] = useState<object>({
+  //   count: 0,
+  //   theme: 'light'
+  // }) inutil
+
+  const incrementCount = () => {
+    setCount((prevState) => prevState + 1);
+  }
+
+  return (
+    <>
+      <div>
+        <h1>{count}</h1>
+
+        <button onClick={incrementCount}>Increment</button>
+      </div>
+    </>
+  )
+}
+
+export default App
