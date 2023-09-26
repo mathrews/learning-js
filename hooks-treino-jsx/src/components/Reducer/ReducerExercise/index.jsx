692913b35a1efb386031fb1c6082093177f7494a
mathrews <mateusps4gg@gmail.com>
2023-09-26 17:56:01 -0300

    código do reduce exercise complementado com uma função de remove e algumas melhorias no código

--- hooks-treino-jsx/src/components/Reducer/ReducerExercise/index.jsx
+++ hooks-treino-jsx/src/components/Reducer/ReducerExercise/index.jsx
@@ -1,65 +1,107 @@
-import { useReducer, useState } from "react";
+import { useEffect, useReducer, useRef, useState } from "react";
 import "./index.css";
 
 const reducer = (state, action) => {
- switch (action.type) {
-  case "new-task": {
-   return {
-    ...state,
-    tasks: action.payload ? [...state.tasks, { nome: action.payload, isCompleted: false }] : [...state.tasks],
-    taskCount: state.taskCount + 1,
-   };
-  }
-  case "toggle": {
-   return {
-    ...state,
-    tasks: state.tasks.map((item, index) =>
-     index == action.payload ? { ...item, isCompleted: !item.isCompleted } : item
-    ),
-   };
-  }
-  default: {
-   return state;
-  }
- }
+    switch (action.type) {
+        case "new-task": {
+            return {
+                ...state,
+                tasks: action.payload
+                    ? [...state.tasks, { nome: action.payload, isCompleted: false }]
+                    : [...state.tasks],
+                taskCount: state.taskCount + 1,
+            };
+        }
+        case "toggle": {
+            return {
+                ...state,
+                tasks: state.tasks.map((item, index) =>
+                    index == action.payload ? { ...item, isCompleted: !item.isCompleted } : item
+                ),
+            };
+        }
+        case "remove": {
+            return{
+                ...state,
+                tasks: state.tasks.filter((item, index) => index != action.payload)
+            }
+        }
+        default: {
+            return state;
+        }
+    }
 };
 
 const ReducerExercise = () => {
- const [state, dispatch] = useReducer(reducer, { tasks: [], taskCount: 0 });
+    const [state, dispatch] = useReducer(reducer, { tasks: [], taskCount: 0 });
 
- const [inputValueTask, setInputValue] = useState("");
+    const [inputValueTask, setInputValue] = useState("");
 
- return (
-  <>
-   <form>
-    <label htmlFor="task">
-     Digite a task:
-     <input
-      onChange={(e) => setInputValue(e.target.value)}
-      type="text"
-      value={inputValueTask}
-     />
-     <button
-      type="submit"
-      onClick={(e) => {
-       e.preventDefault();
-       dispatch({ type: "new-task", payload: inputValueTask });
-       setInputValue("")
-      }}>
-      Adicionar task
-     </button>
-     {state.tasks.map((item, index) => (
-      <li
-       key={index}
-       onClick={() => dispatch({ type: "toggle", payload: index })}
-       style={{ textDecoration: item.isCompleted ? "line-through" : "none" }}>
-       {item.nome}
-      </li>
-     ))}
-    </label>
-   </form>
-  </>
- );
+    const renders = useRef(0);
+
+    useEffect(() => {
+        renders.current += 1;
+    }, [state]);
+
+    // const removeTask = (indexTask) => {
+    //     let aux = []
+    //     state.tasks.map((item, index) => {
+    //         if (index != indexTask) {
+    //             aux.push(item)
+    //         }
+    //     })
+    //     state.tasks = aux
+    // }
+
+    return (
+        <>
+            <form>
+                <label htmlFor="task">
+                    Digite a task:
+                    <input
+                        onChange={(e) => setInputValue(e.target.value)}
+                        type="text"
+                        value={inputValueTask}
+                        id="task"
+                    />
+                </label>
+                <button
+                    type="submit"
+                    onClick={(e) => {
+                        e.preventDefault();
+                        dispatch({ type: "new-task", payload: inputValueTask });
+                        setInputValue("");
+                    }}>
+                    Adicionar task
+                </button>
+            </form>
+
+            <ul>
+                {state.tasks.map((item, index) => (
+                    <>
+                        <li
+                            key={index}
+                            style={{
+                                textDecoration: item.isCompleted ? "line-through" : "none",
+                                display: "flex",
+                                flexDirection: "column",
+                                width: 100,
+                                gap: 6,
+                                margin: 6
+                            }}>
+                            <div style={{ display: "flex", alignItems: "center", gap: 6 }}>
+                                {item.nome}{" "}
+                                <button onClick={() => dispatch({ type: "toggle", payload: index })}>toggle</button>
+                                <button onClick={() => dispatch({ type: "remove", payload: index })}>Remove</button>
+                            </div>
+                        </li>
+                    </>
+                ))}
+            </ul>
+
+            <h2>{renders.current}</h2>
+        </>
+    );
 };
 
 export default ReducerExercise;
