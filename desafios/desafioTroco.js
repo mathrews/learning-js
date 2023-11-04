ecaba50635e5e6dfe2f7f49021fe236d84dc1120
mathrews <mateusps4gg@gmail.com>
2023-11-04 02:02:16 -0300

    algorítmo concluído com ajuda divina

--- desafios/desafioTroco.js
+++ desafios/desafioTroco.js
@@ -1,8 +1,8 @@
 const calcTroco = (productValue, money) => {
 
-    const cedulas = [200, 100, 50, 20, 5, 1, 0.50, 0.10, 0.05, 0.01];
+    const cedulasMoedas = [200, 100, 50, 20, 10, 5, 2, 1, 0.50, 0.25, 0.10, 0.05, 0.01];
 
-    const troco = money - productValue;
+    let troco = (money - productValue).toFixed(2);
 
     if (troco < 0) {
         return "dinheiro insuficiente";
@@ -10,13 +10,40 @@ const calcTroco = (productValue, money) => {
         return "valor completo";
     }
 
-    let cedulasDoTroco = [];
+    let resto = troco;
+    let resultados = []
 
-    cedulas.map((item) => {
-        cedulasDoTroco.push((troco / item).toFixed(2));
-    })
+    for (let valor of cedulasMoedas) {
 
-    return cedulasDoTroco
+        const quantidade = Math.floor(resto / valor);
+
+        if (quantidade === 0) continue;
+
+        resultados.push({ quantidade, valor })
+
+        resto = resto % valor;
+    }
+
+    return ({ troco, resultados });
+
 }
 
-console.log(calcTroco(12.1, 61));
+const elaborarResultado = (func) => {
+    let resultadoEmTexto = '';
+
+    if (func === "dinheiro insuficiente") {
+        return func
+    }
+
+    func.resultados.map((item) => {
+        if (item.quantidade > 0 && item.valor >= 1) {
+            resultadoEmTexto += ` ${item.quantidade.toLocaleString('pt-BR')} nota(s) de R$${item.valor.toLocaleString('pt-BR')},`;
+        } else if (item.quantidade > 0 && item.valor <= 1) {
+            resultadoEmTexto += ` ${item.quantidade.toLocaleString('pt-BR')} moeda(s) de R$${item.valor.toLocaleString('pt-BR')},`
+        }
+    })
+
+    return `Troco para R$${func.troco}:${resultadoEmTexto}`.replace(/.$/, ".");
+}
+
+console.log(elaborarResultado(calcTroco(10, 40)));
